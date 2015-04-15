4c8aa41af2eac5d109f17ee4d33d43a9875db6b7
Rakhi Pal <rakhi.pal@pubmatic.com>
2015-04-15 13:14:34 +0530

    Adding app folder and configuration files to git.

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.repeatData = ["rakhi","is ","learning","angular","js","0","0","8","9"];
+    $scope.statusList = ["incorrect","wrong ","stuff"];
+  });
